52b5e4786707ad348c8b01e2ddfdc725cf76e526
Oleksii Makieiev <alexmakeev2703@gmail.com>
2025-10-31 23:29:05 +0200

    Add the starter of the application

---
+++ backend/lined/src/main/java/io/backend/lined/LinedBackendApplication.java
@@ -0,0 +1,13 @@
+package io.backend.lined;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinedBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinedBackendApplication.class, args);
+	}
+
+}
---
+++ backend/lined/src/test/java/io/backend/lined/LinedBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.backend.lined;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinedBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
