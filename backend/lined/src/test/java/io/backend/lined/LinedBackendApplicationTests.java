e8ae24ce959404130a33a2707f3bdc90fad12dd9
Oleksii Makieiev <alexmakeev2703@gmail.com>
2026-01-17 16:22:45 +0200

    Configure test profile for application tests

--- backend/lined/src/test/java/io/backend/lined/LinedBackendApplicationTests.java
+++ backend/lined/src/test/java/io/backend/lined/LinedBackendApplicationTests.java
@@ -2,12 +2,14 @@ package io.backend.lined;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class LinedBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }
